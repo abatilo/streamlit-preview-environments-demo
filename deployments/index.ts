dbed49dd4c4de7896d42596925b4a92b1d909af8
Aaron <AaronBatilo@gmail.com>
2020-09-18 20:01:13 -0600

    Create a gateway to be used by all virtualservices

--- deployments/index.ts
+++ deployments/index.ts
@@ -4,7 +4,7 @@ import * as k8s from "@pulumi/kubernetes";
 const clusterStackRef = new pulumi.StackReference("prod");
 const kubeconfig = clusterStackRef.getOutput("kubeconfig");
 const k8sProvider = new k8s.Provider("prod", {
-  kubeconfig: kubeconfig.apply(JSON.stringify)
+  kubeconfig: kubeconfig.apply(JSON.stringify),
 });
 
 const appName = "streamlit-preview";
@@ -15,36 +15,36 @@ const ns = new k8s.core.v1.Namespace(
     metadata: {
       name: appName,
       labels: {
-        "istio-injection": "enabled"
-      }
-    }
+        "istio-injection": "enabled",
+      },
+    },
   },
   { provider: k8sProvider }
 );
 
-// const gateway = new k8s.apiextensions.CustomResource(
-//   appName,
-//   {
-//     apiVersion: "networking.istio.io/v1alpha3",
-//     kind: "Gateway",
-//     metadata: {
-//       namespace: deployment.metadata.namespace
-//     },
-//     spec: {
-//       selector: {
-//         istio: "ingressgateway"
-//       },
-//       servers: [
-//         {
-//           port: {
-//             number: 443,
-//             name: "https",
-//             protocol: "HTTP"
-//           },
-//           hosts: ["www.aaronbatilo.dev"]
-//         }
-//       ]
-//     }
-//   },
-//   { provider: k8sProvider }
-// );
+const gateway = new k8s.apiextensions.CustomResource(
+  appName,
+  {
+    apiVersion: "networking.istio.io/v1alpha3",
+    kind: "Gateway",
+    metadata: {
+      namespace: appName,
+    },
+    spec: {
+      selector: {
+        istio: "ingressgateway",
+      },
+      servers: [
+        {
+          port: {
+            number: 443,
+            name: "https",
+            protocol: "HTTP",
+          },
+          hosts: ["*.streamlit.aaronbatilo.dev"],
+        },
+      ],
+    },
+  },
+  { provider: k8sProvider }
+);
