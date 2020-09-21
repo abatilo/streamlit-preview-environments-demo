667ab16bc8f845a64b0852548ea1730cef843393
Aaron Batilo <AaronBatilo@gmail.com>
2020-09-21 16:35:25 -0600

    Update main.py

--- streamlit-application/main.py
+++ streamlit-application/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-starting_point = st.text_input('How much money are you starting with?', value='1000')
-interest = st.slider('What is your interest rate percentage?', min_value=1, max_value=20, value=9)
-years = st.slider('How many years are you waiting?', min_value=1, value=30)
+starting_point = st.text_input('How much money are you starting with?', value='10000')
+interest = st.slider('What is your interest rate percentage?', min_value=1, max_value=20, value=12)
+years = st.slider('How many years are you waiting?', min_value=1, value=45)
 
 def compound(P, r, t):
   return f'{(int(P) * (1 + (float(r) / 100.0)) ** float(t)):,.2f}'
