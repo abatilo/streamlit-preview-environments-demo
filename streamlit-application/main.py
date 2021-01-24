b60a200441b07689d47c51fc1058896422558fdf
Aaron Batilo <AaronBatilo@gmail.com>
2021-01-24 11:36:11 -0700

    Update main.py

--- streamlit-application/main.py
+++ streamlit-application/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.text('Hello there')
+st.text('Hello asdf')
 
 starting_point = st.text_input('How much are you starting with?', value='20000')
 interest = st.slider('What is your interest rate percentage?', min_value=1, max_value=20, value=12)
